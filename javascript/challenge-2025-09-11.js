19ca7f0b07c11bb4156d47836c2c8d698d26aba3
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-09-13 14:06:24 -0500

    Challenge 2025-09-11: Reformat test and add test case.

--- javascript/challenge-2025-09-11.js
+++ javascript/challenge-2025-09-11.js
@@ -1,21 +1,23 @@
 function reverseSentence(sentence) {
   const word_list = sentence.split(/\s+/);
-  console.log("word_list (1):", word_list);
+  // console.log("word_list (1):", word_list);
   word_list.reverse();
-  console.log("word_list (2):", word_list);
+  // console.log("word_list (2):", word_list);
   let result = word_list.join(" ");
-  console.log("result:", result);
+  // console.log("result:", result);
   return result;
 }
 
 function test_io(input) {
   let output = reverseSentence(input);
-  console.log("input:", input);
-  console.log("output:", output);
+  console.log("Testing reverseSentence:");
+  console.log(" - input:", input);
+  console.log(" - output:", output);
 }
 
 function main() {
   test_io("world hello");
+  test_io("push commit git");
 }
 
 if (require.main == module) {
--- python/challenge-2025-09-11.py
+++ python/challenge-2025-09-11.py
@@ -6,11 +6,13 @@ def reverse_sentence(sentence):
 
 def test_io(input):
     output = reverse_sentence(input)
-    print(f"input: {input}")
-    print(f"output: {output}")
+    print("Testing reverse_sentence:")
+    print(f" - input: {input}")
+    print(f" - output: {output}")
 
 def main():
     test_io("world hello")
+    test_io("push commit git")
 
 if __name__ == "__main__":
     main()
