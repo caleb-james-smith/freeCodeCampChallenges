102986c57b65a31e12eefbef47a812557987dfd1
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-09-13 13:55:24 -0500

    Challenge 2025-09-11: Test JavaScript solution.

---
+++ javascript/challenge-2025-09-11.js
@@ -0,0 +1,23 @@
+function reverseSentence(sentence) {
+  const word_list = sentence.split(/\s+/);
+  console.log("word_list (1):", word_list);
+  word_list.reverse();
+  console.log("word_list (2):", word_list);
+  let result = word_list.join(" ");
+  console.log("result:", result);
+  return result;
+}
+
+function test_io(input) {
+  let output = reverseSentence(input);
+  console.log("input:", input);
+  console.log("output:", output);
+}
+
+function main() {
+  test_io("world hello");
+}
+
+if (require.main == module) {
+  main();
+}
