5fa249e8dd6c638273025e8c35e8f9d4e1313ab7
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-10-05 06:02:44 -0400

    Use CodeTester to test challenge 2025-10-02.

--- python/challenge-2025-10-02.py
+++ python/challenge-2025-10-02.py
@@ -1,8 +1,23 @@
+from CodeTester import CodeTester
 
 def to_binary(decimal):
     result = bin(decimal)
     result = result[2:]
     return result
 
+def main():
+    tests = [
+        {"input"    : 5,
+         "expected" : "101"},
+        {"input"    : 12,
+         "expected" : "1100"},
+        {"input"    : 50,
+         "expected" : "110010"},
+        {"input"    : 99,
+         "expected" : "1100011"}
+    ]
+    tester = CodeTester(to_binary, tests)
+    tester.runTests()
 
-
+if __name__ == "__main__":
+    main()
